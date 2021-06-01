f6364dfb05626948f88ebdd7389d4831224709a9
Anshul Pandey <anshulpandey2@outlook.com>
2021-06-01 12:05:52 +0530

    Update __init__.py

--- coreAPI/__init__.py
+++ coreAPI/__init__.py
@@ -1,19 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-# from coreAPI.models import Post
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-user ='postgres'
-pw ='1148'
-dbase ='test'
-host ='localhost'
-port = '5432'
 
-
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{user}:{pw}@{host}:{port}/{dbase}"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
